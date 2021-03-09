c0cd615bc08ac0585d6ce4419e21d65bcdd3bbd0
GeekHolo <artem.avetis@yande.ru>
2021-03-09 20:09:56 +0300

    Лёгкая редактура

--- CrossInformTest/CrossInformTest/Program.cs
+++ CrossInformTest/CrossInformTest/Program.cs
@@ -21,53 +21,54 @@ namespace CrossInformTest
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
+            Stopwatch _stopWatch = new Stopwatch();
 
             try
             {
                 Console.WriteLine("Укажите путь к вашему файлу с его названием в формате: C:\\Users\\1\\Documents\\vlastelin_kolec_bratstvo_kol_ca.txt");
+                
                 int _maxValue = 0; //наибольшее число повторов среди триплетов
-
-                string path = Console.ReadLine();
-                stopWatch.Start();//запуск таймера работы программы
+                char[] punctuation_marks = { '.', ',', '!', '?', ';', ':', '-','>','<',')','(',' ', '»', '«',' '};
+                string _path = Console.ReadLine();
+                _stopWatch.Start();//запуск таймера работы программы
              
-                string[] text = File.ReadAllLines($@"{path}");//формирование массива строк из указанного файла
+                string[] text = File.ReadAllLines($@"{_path}");//формирование массива строк из указанного файла
                 
                 Dictionary<string, int> DictionaryOfTriplets = new Dictionary<string, int>();//объявление словоря, где триплет-это ключ,а количество его повторов - значение
-
 
                 foreach (var line in text)
                 {
                     string[] words = line.Split(' ');//разбиение строк на слова и занесение их в массив строк
+                    
                     foreach (var word in words)
-                    {   
-                        char[] wordAsArray = word.ToLower().ToCharArray();//разбиение слова на буквы-символы и перевод их в нижний регистр с последующим занесением в массив
-                        
-                        if (wordAsArray.Length >= 3 )
+                    {
+                        char[] wordAsArray = word.ToLower().Trim(punctuation_marks).ToCharArray();//слово в виде массива букв
+
+                        if (wordAsArray.Length >= 3)
                         {
-                            
                             for (int j = 0; j <= wordAsArray.Length-3; j++)//формирование триплетов из символов
                             {
                                 string triplet = null;
                                 
                                 for (int i = j; i <j+ 3; i++)
-                                {
+                                {                                    
                                         triplet += wordAsArray[i].ToString();
+
                                 }
 
                                 if (DictionaryOfTriplets.ContainsKey(triplet) != true)//проверка наличия полученного триплета в словаре триплетов
                                 {
-                                    
+
                                     DictionaryOfTriplets.Add(triplet, 1);//если он новый, то добавляется в словарь со значением 1
-  
+
                                 }
                                 else
                                 {
-                                    DictionaryOfTriplets[triplet] +=1;//если уже присутствует, его значение в словаре увеличится на единицу
+                                    DictionaryOfTriplets[triplet] += 1;//если уже присутствует, его значение в словаре увеличится на единицу
                                     if (DictionaryOfTriplets[triplet] > _maxValue)
                                         _maxValue = DictionaryOfTriplets[triplet];
                                 }
-
+                                
                             }
                             
 
@@ -76,9 +77,9 @@ namespace CrossInformTest
                     }
                     
                 }
-                
-               
-                _maxValue = MaxValue(_maxValue, DictionaryOfTriplets)+1;
+
+        
+              //  _maxValue = MaxValue(_maxValue, DictionaryOfTriplets)+1;
                 int _counterOfCycles = 0;
                 _maxValue++;//увеличиваю максимальное значение на 1, так как в последующем цикле оно будет уменьшено на 1
                 while (_counterOfCycles < 10)//вывод 10 триплетов с наибольшим числом повторов в тексте
@@ -94,14 +95,12 @@ namespace CrossInformTest
                                 if (_counterOfCycles == 9)
                                 {
                                    Console.WriteLine($"{key}.");
-                                   _counterOfCycles++;
                                 }
                                 else
                                 {
                                    Console.Write($"{key},");
-                                   _counterOfCycles++;
                                 }
-
+                                _counterOfCycles++;
                             }
 
                         }
@@ -111,10 +110,11 @@ namespace CrossInformTest
 
             }
             catch (Exception e) { Console.WriteLine(e.Message); }
-            finally{
-                stopWatch.Stop();
+            finally
+            {
+                _stopWatch.Stop();
                 // Get the elapsed time as a TimeSpan value.
-                TimeSpan ts = stopWatch.Elapsed;
+                TimeSpan ts = _stopWatch.Elapsed;
 
                 // Format and display the TimeSpan value.
                 string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
@@ -125,15 +125,6 @@ namespace CrossInformTest
             }
             
             Console.ReadKey();
-        }
-        public static int MaxValue(int _maxValue, Dictionary<string, int> keyValuePairs)//функция для поиска наибольшего числа повторов среди триплетов
-        {
-            foreach (string key in keyValuePairs.Keys)
-            {
-                if (keyValuePairs[key] > _maxValue)
-                    _maxValue = keyValuePairs[key];
-            }
-            return _maxValue;
         }
 
     }
